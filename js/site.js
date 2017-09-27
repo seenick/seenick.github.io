ebe14377a09fcabdfd8675b7e4c82d5b34910e41
Nick Christensen <nchristensen@Nicks-MacBook-Pro.local>
2017-09-27 11:04:14 -0500

    add beta folder

--- js/site.js
+++ js/site.js
@@ -6,40 +6,44 @@ $(document).ready(function() {
 
 	$('.caption').show();
 
-
 // =============================================================================
 //	Background image
 // =============================================================================
 
-	$('.has-bg').each(function(){
+$('.has-bg').each(function(){
+
+	if ( $(this).find('img').length > 0 ) {
 		var imgSrc = $(this).find('img').attr('src');
 
 		$(this).css('background-image', 'url(' + imgSrc + ')');
 		$(this).find('img').hide();
-	});
+	}
+
+});
 
 // =============================================================================
 //	Project Toggle
 // =============================================================================
 
-	 $('.tab').on("click", function(){
-	 	$(this).parent('.project').toggleClass('open');
-	 	$(this).parent('.project').siblings().removeClass('open');
-	 });
+$('.tab').on("click", function(){
+	$(this).parent('.project').toggleClass('open');
+	$(this).parent('.project').siblings().removeClass('open');
+});
 
 // =============================================================================
 //	Photo Showcase
 // =============================================================================
 
-	$('.showcase').on("click", function(){
-		$(this).find('img').clone().appendTo('.showcase-screen');
-		$('.showcase-screen').addClass('open');
-	});
+$('.showcase').on("click", function(){
+	$(this).find('img').clone().appendTo('.showcase-screen');
+	$('.showcase-screen').addClass('open');
+});
 
-	$('.showcase-screen').on("click", function(){
-		$(this).removeClass('open');
-		$(this).find('img').remove();
-	});
+$('.showcase-screen').on("click", function(){
+	$(this).removeClass('open');
+	$(this).find('img').remove();
+});
+
 
 }); // end document ready
 
@@ -48,8 +52,8 @@ $(document).ready(function() {
 //	Document Keyup
 // =============================================================================
 $(document).keyup(function(e) {
-  if (e.keyCode == 27) {
-    $('.showcase-screen').removeClass('open');
-    $('.showcase-screen').find('img').remove();
-  }
+	if (e.keyCode == 27) {
+		$('.showcase-screen').removeClass('open');
+		$('.showcase-screen').find('img').remove();
+	}
 });
