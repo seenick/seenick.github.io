2304223bff2ed64d0b559b245765210762bcfc05
Nick Christensen <nickchristensen@Nicks-MacBook-Pro.local>
2020-02-16 16:57:17 -0600

    changes

--- js/site.js
+++ js/site.js
@@ -1,27 +1,18 @@
-/*
-** pagePiling
-*/
+// /*
+// ** pagePiling
+// */
 
-$(document).ready(function() {
-	$('#pagepiling').pagepiling({
-		navigation: {
-            'textColor': '#FFFFFF',
-            'bulletsColor': '#FFFFFF',
-            'position': 'right'
-        },
-        anchors: ['welcome','about','skills','work','work2','work3','work4'],
-        css3: true,
-        loopBottom: true,
-    	loopTop: true,
-    	keyboardScrolling: true
-	});
-});
-
-$(document).ready(function() {
-	$('#pp-nav ul li').each(function(){
-		$(this).on('click',function(){
-			$(this).addClass('is-active');
-			$(this).siblings().removeClass('is-active');
-		});
-	});
-});
+// $(document).ready(function() {
+// 	$('#pagepiling').pagepiling({
+// 		navigation: {
+//             'textColor': '#FFFFFF',
+//             'bulletsColor': '#FFFFFF',
+//             'position': 'right'
+//         },
+//         anchors: ['welcome','about','skills','work','work2','work3','work4'],
+//         css3: true,
+//         loopBottom: true,
+//     	loopTop: true,
+//     	keyboardScrolling: true
+// 	});
+// });
