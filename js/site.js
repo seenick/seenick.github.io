087c76a7a716f546189bed4306a4b1590498964b
Nick Christensen <nickchristensen@Nicks-MacBook-Pro.local>
2020-02-19 11:19:38 -0600

    offcanvas nav functions

--- js/site.js
+++ js/site.js
@@ -13,3 +13,32 @@ $(document).ready(function () {
   	slidesPerView: 5,
   });
 });
+
+
+/*
+** offcanvas menu
+*/
+
+$('.menu-trigger').attr('aria-expanded','false');
+	$('.offcanvas-menu').attr('aria-expanded','false');
+
+	$('.menu-trigger').on('click',function(){
+		$('.offcanvas-menu').addClass('is-visible');
+		$('.offcanvas-menu').attr('aria-expanded','true');
+		$('.menu-trigger').attr('aria-expanded','true');
+	});
+
+	$('.close-trigger').on('click',function(){
+		$('.offcanvas-menu').removeClass('is-visible');
+		$('.offcanvas-menu').attr('aria-expanded','false');
+		$('.menu-trigger').attr('aria-expanded','false');
+	});
+
+	$(document).keyup(function(e) {
+		// esc
+		if (e.keyCode === 27)  {
+			$('.offcanvas-menu').removeClass('is-visible');
+			$('.offcanvas-menu').attr('aria-expanded','false');
+			$('.menu-trigger').attr('aria-expanded','false');
+		} 
+	});
