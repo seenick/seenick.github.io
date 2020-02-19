6444020e5957327403f19d653f3ea3267fe21858
Nick Christensen <nickchristensen@Nicks-MacBook-Pro.local>
2020-02-19 13:39:37 -0600

    swiperjs looping

--- js/site.js
+++ js/site.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
   //initialize swiper when document ready
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
+    loop: true,
     navigation: {
     	nextEl: '.swiper-button-next',
     	prevEl: '.swiper-button-prev',
   	},
   	slidesPerView: 5,
+  	loopedSlides: 6
   });
 });
 
