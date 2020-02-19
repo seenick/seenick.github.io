9f11f8d74b8e26192356045c1d218497310233f7
Nick Christensen <nickchristensen@Nicks-MacBook-Pro.local>
2020-02-19 09:52:40 -0600

    project information

--- js/site.js
+++ js/site.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
   //initialize swiper when document ready
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
-    loop: true,
     navigation: {
     	nextEl: '.swiper-button-next',
     	prevEl: '.swiper-button-prev',
